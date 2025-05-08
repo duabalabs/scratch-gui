0cbac983ffb1676246127f9a2352b47297b634e5
Abel Tattah <122866505+AbelTattah@users.noreply.github.com>
2025-05-08 03:49:25 +0000

    fix: patch to fix build errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+import 'regenerator-runtime/runtime';
 import GUI from './containers/gui.jsx';
 import AppStateHOC from './lib/app-state-hoc.jsx';
-import GuiReducer, {guiInitialState, guiMiddleware, initEmbedded, initFullScreen, initPlayer} from './reducers/gui';
-import LocalesReducer, {localesInitialState, initLocale} from './reducers/locales';
-import {ScratchPaintReducer} from 'scratch-paint';
-import {setFullScreen, setPlayer} from './reducers/mode';
-import {remixProject} from './reducers/project-state';
-import {setAppElement} from 'react-modal';
-import {activateDeck} from './reducers/cards.js';
+import GuiReducer, { guiInitialState, guiMiddleware, initEmbedded, initFullScreen, initPlayer } from './reducers/gui';
+import LocalesReducer, { localesInitialState, initLocale } from './reducers/locales';
+import { ScratchPaintReducer } from 'scratch-paint';
+import { setFullScreen, setPlayer } from './reducers/mode';
+import { remixProject } from './reducers/project-state';
+import { setAppElement } from 'react-modal';
+import { activateDeck } from './reducers/cards.js';
 
 const guiReducers = {
     locales: LocalesReducer,
